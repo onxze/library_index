2fe69aebf6b87094649857553cdc79cdcb20895b
Mika Hatakka <Mika.Hatakka@diferion.org>
2014-10-19 19:29:49 +0300

    Added checks and error handling.

--- LibraryIndexApi.php
+++ LibraryIndexApi.php
@@ -5,34 +5,104 @@
  */
 class LibraryIndexApi {
 
-  private $urlOpenHours = 'http://api.kirjastot.fi/v2/';
   private $apiUrl;
+  private $httpStatus;
+  private $code;
+  private $errorMessage;
+
+  const LIA_NO_ERROR = 0;
+  const NO_API_URL = 1;
+  const LIA_CURL_FAIL = 2;
+  const LIA_NOT_200 = 3;
 
   /**
    * Inject api url to class.
    * @param type $apiUrl
    */
   public function __construct($apiUrl) {
-    if (empty($apiUrl)) {
-      $this->apiUrl = $this->urlOpenHours;
-    }
-    else {
-      $this->apiUrl = $apiUrl;
-    }
+    $this->apiUrl = $apiUrl;
   }
 
   /**
    * Read Library's Open Hours from kirjastot.fi api.
-   * @param type $lid
-   * @return type
+   * @param type $lid Library id at kirjastot.fi
+   * @param type $firstDate start date of open hours.
+   * @return type returned response or NULL
    */
-  public function getOpenHours($lid) {
+  public function getOpenHours($lid, $firstDate = NULL) {
+    $date = '';
+
+    if (!empty($firstDate)) {
+      $lastDate = $firstDate + (6 * 24 * 60 * 60);
+      $date = '&date>=' . date('Y-m-d', $firstDate) .
+              '&date<=' . date('Y-m-d', $lastDate);
+    }
+
+    $query = 'schedules/' . $lid . '?as_weeks=1' . $date;
+    $responseAsObject = $this->queryData($query);
+    return $responseAsObject;
+  }
+
+  /**
+   * Return last query http result code
+   * @return int HTTP status
+   */
+  public function getHttpStatus() {
+    return $this->httpStatus;
+  }
+
+  /**
+   * Return last query error code
+   * @return int error code
+   */
+  public function getErrorCode() {
+    return $this->code;
+  }
+
+  /**
+   * Returnn last query error message
+   * @return string error message
+   */
+  public function getErrorMessage() {
+    return $this->errorMessage;
+  }
+
+  /**
+   * Set error code and message.
+   * @param int $code error code
+   * @param string $errorMessage error message
+   */
+  private function setError($code, $errorMessage) {
+    $this->code = $code;
+    $this->errorMessage = $errorMessage;
+  }
+
+  /**
+   * Clear last query errors.
+   */
+  private function clearError() {
+    $this->code = 'LIA_NO_ERROR';
+    $this->errorMessage = NULL;
+  }
+
+  /**
+   * Ask data from kirjastot.fi Library directory
+   * @param type $query query to kirjastot.fi Library directory api
+   * @return type returned response or NULL
+   */
+  private function queryData($query) {
+    $this->clearError();
+    if (empty($this->apiUrl)) {
+      $this->setError('LIA_NO_API_URL', t('No Api URL'));
+      return NULL;
+    }
+    $fullURL = $this->apiUrl . 'libraries/' . $query;
+    $this->httpStatus = 0;
+
     $curl = curl_init();
     curl_reset($curl);
-  
-    $fullURL = $this->apiUrl . 'libraries/schedules/' . $lid . '?as_weeks=1';
-    curl_setopt($curl, CURLOPT_URL, $fullURL);
 
+    curl_setopt($curl, CURLOPT_URL, $fullURL);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
     $headers = array(
@@ -41,17 +111,21 @@ class LibraryIndexApi {
     );
 
     array_push($headers, "Accept: application/json");
-
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
     $response = curl_exec($curl);
 
-    //Error: something caused by
     if (curl_errno($curl)) {
-      dpm('Curl error: ' . curl_error($curl), curl_errno($curl));
+      $this->setError('LIA_CURL_FAIL', t('Curl error') . ': ' . curl_error($curl), curl_errno($curl));
+      return NULL;
     }
 
-    $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $this->httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    if ($this->httpStatus != 200) {
+      $this->setError('LIA_NOT_200', t('HTTP status not 200'));
+      return NULL;
+    }
 
     $responseAsObject = json_decode($response);
 
