405d5dbcbe9e4b38f7fd9c160bdd348e90b7283c
Mika Hatakka <Mika.Hatakka@diferion.org>
2014-12-30 13:21:31 +0200

    Fixed block template marking.

--- li_open_hours/li-open-hours-block.tpl.php
+++ li_open_hours/li-open-hours-block.tpl.php
@@ -26,4 +26,5 @@
   <div>
     <span class="loh-prev"><a href="<?php echo $prev_link_url; ?>" class="<?php echo $prev_link_class; ?>"><?php echo t('Previous'); ?></a></span>
     <span class="loh-next"><a href="<?php echo $next_link_url; ?>" class="<?php echo $next_link_class ?>"><?php echo t('Next'); ?></a></span>
+  </div>
 </div>
